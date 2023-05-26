ae2c0f4635edf0ef04cc66b2d1f842ba58b97612
bloodnighttw <bbeenn1227@gmail.com>
2023-05-26 18:45:45 +0800

    Remove some useless code.

--- import/nav.php
+++ import/nav.php
@@ -10,7 +10,7 @@
             </ul>
         </div>
 
-        <div class="center-text" ST>
+        <div class="center-text">
             <p class="title"><?php echo $page_func ?? "DEFINED" ?></p>
         </div>
 
